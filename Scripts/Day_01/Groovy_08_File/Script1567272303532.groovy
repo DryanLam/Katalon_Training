d7f6177d40f8147b15765a4d4ce03b22101d4e4c
dlam <dryanlam@gmai.com>
2021-09-20 16:16:23 +0700

    IO

--- Scripts/Day_01/Groovy_08_File/Script1567272303532.groovy
+++ Scripts/Day_01/Groovy_08_File/Script1567272303532.groovy
@@ -22,25 +22,25 @@ new File(".\\Test Cases\\Day_01\\test.txt").withWriter('utf-8') {
 
 // Session 3
 // Append the file
-def file = new File(".\\Test Cases\\Day_01\\test.txt")
-file.append('This message append to the file')
+def file_01 = new File(".\\Test Cases\\Day_01\\test.txt")
+file_01.append('This message append to the file')
 
 // Session 4
 // Get the file as a string
-def file = new File(".\\Test Cases\\Day_01\\test.txt")
-println(file.text)
+def file_02 = new File(".\\Test Cases\\Day_01\\test.txt")
+println(file_02.text)
 
 // Get the file size
 // >> Should check file size in property
-println("File Size : ${file.length()} bytes")
+println("File Size : ${file_02.length()} bytes")
 
 // Check if a file or directory
-println("File : ${file.isFile()}")
-println("Dir : ${file.isDirectory()}")
+println("File : ${file_02.isFile()}")
+println("Dir : ${file_02.isDirectory()}")
 
 // Copy file to another file
 def newFile = new File(".\\Test Cases\\Day_01\\test_copy.txt")
-newFile << file.text
+newFile << file_02.text
 
 // Delete a file
 newFile.delete()
